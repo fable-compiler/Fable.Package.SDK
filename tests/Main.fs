3dfe5f11c5e3637e6622a67b959b60035a547261
Maxime Mangel <mangel.maxime@protonmail.com>
2024-10-20 12:44:34 +0200

    fix: workaround limitations in VS and Rider
    Fix #9

--- tests/Main.fs
+++ tests/Main.fs
@@ -2,10 +2,9 @@ module Fable.Package.SDK.Tests
 
 // open Microsoft.VisualStudio.TestTools.UnitTesting
 open SimpleExec
-open System.IO
-open System.Threading.Tasks
-open NUnit
 open NUnit.Framework
+open Thoth.Json.Core
+open Thoth.Json.Newtonsoft
 
 let private expectToFailWithMessage projectName (message: string) =
     task {
@@ -222,21 +221,62 @@ let ``should support multiple fable targets`` () =
         )
     }
 
+type MSBuildItem =
+    {
+        FullPath: string
+        Pack: bool
+    }
+
+    static member Decoder =
+        Decode.object (fun get ->
+            {
+                FullPath = get.Required.Field "FullPath" Decode.string
+                Pack =
+                    get.Required.Field
+                        "Pack"
+                        (Decode.string
+                         |> Decode.andThen (
+                             function
+                             | "true" -> Decode.succeed true
+                             | "false" -> Decode.succeed false
+                             | _ -> Decode.fail "Invalid boolean value"
+                         ))
+            }
+        )
+
 [<Test>]
 let ``should include the source file and the project file under 'fable' folder`` () =
     task {
         let! stdout, _ =
             Command.ReadAsync(
                 "dotnet",
-                $"msbuild %s{Workspace.fixtures.valid.``library-with-files``.``MyLibrary.fsproj``} --getItem:Content"
+                $"msbuild %s{Workspace.fixtures.valid.``library-with-files``.``MyLibrary.fsproj``} --getItem:None --getItem:Compile"
             )
 
+        let projectFile =
+            Decode.unsafeFromString
+                (Decode.at [ "Items"; "None" ] (Decode.list MSBuildItem.Decoder))
+                stdout
+            // We are only interested in the project file
+            |> List.filter (fun item -> item.FullPath.Contains("MyLibrary.fsproj"))
+            |> List.head
+
+        let compileItem =
+            Decode.unsafeFromString
+                (Decode.at [ "Items"; "Compile" ] (Decode.index 0 MSBuildItem.Decoder))
+                stdout
+
+        Assert.That(compileItem.Pack, Is.True)
+
         Assert.That(
-            stdout.Trim(),
+            compileItem.FullPath,
             Contains.Substring("tests/fixtures/valid/library-with-files/Entry.fs")
         )
+
+        Assert.That(projectFile.Pack, Is.True)
+
         Assert.That(
-            stdout.Trim(),
+            projectFile.FullPath,
             Contains.Substring("tests/fixtures/valid/library-with-files/MyLibrary.fsproj")
         )
     }
